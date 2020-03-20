21c29075d193696701f800826ba9c0019c88bc05
Taoma-china <60022761+Taoma-china@users.noreply.github.com>
2020-03-20 15:49:44 -0400

    Update ali2.py

--- ali2.py
+++ ali2.py
@@ -1,3 +1,15 @@
+
+
+#题目大概是这样的，小明在学旋律，每段旋律都可以用字符串来表示，并且旋律的每个
+#字符的ASCALL码递增的比如以下4段旋律 :
+#aaa    bcd    bcdef    zzz    
+#现在就是要求，小明能够吧这些旋律拼接起来组成最长的旋律。
+#比如上述例子输出 11 最长的旋律为 aaabcdefzzz 
+
+
+
+
+
 import numpy as np
 def longest(arr):
 	new_arr =[]
